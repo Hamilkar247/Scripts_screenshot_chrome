456f6a0e560347c0304007e3c5aea4b5142c38da
Hamilkar247 <adonnibal96@gmail.com>
2022-09-08 11:32:08 +0200

    [WO-3] wystepowal blad zbyt duzej ilosci logow oraz blad sesji - zamiast obslugiwac blad go tylko łapałem a ptela dziala dalej
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- screenShot.py
+++ screenShot.py
@@ -6,15 +6,15 @@ from requests import Session
 from selenium import webdriver
 from pyvirtualdisplay import Display
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 import logging
 import traceback
 
-#logi
-logging.basicConfig(level=logging.DEBUG)
+#logi - gdy wystąpią błędy - odkomentuj - będą wyświetlane także logi związane wewnetrznie z uzytymi libkami
+logging.basicConfig(level=logging.INFO)#logging.DEBUG)
 
 display = Display(visible=0, size=(1920, 1200))
 display.start()
-
 
 #Driver = 'chromedriver'
 #driver = webdriver.Chrome(Driver)
@@ -30,69 +30,73 @@ chrome_options.add_argument("--headless")
 
 
 driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver',chrome_options=chrome_options)
-
 driver.set_window_size(1920,1316)
-
 driver.set_script_timeout(30)
 driver.set_page_load_timeout(30) # seconds
 
-while True:
-    try:
+try:
+    while True:
+        logging.info(f"numer sesji {driver.session_id}")
         milli_sec = int(round(time.time()*1000))
         now = datetime.now()
         t = now.strftime("[%Y/%m/%d-%H:%M:%S]")
         driver.get('https://czujnikimiejskie.pl/public/kolno/?url_node_id=281&a='+str(milli_sec))
         time.sleep(20)
         screenshot = driver.save_screenshot('/home/devel/kolno_map.png')
-        print(t, " ScreenShoot: Mapa Kolno")
-        
+        logging.info("ScreenShoot: Mapa Kolno")
+          
         now = datetime.now()	
         t = now.strftime("[%Y/%m/%d-%H:%M:%S]")
         driver.get('https://czujnikimiejskie.pl/public/kozienice/?url_node_id=196&kml=false&a='+str(milli_sec))
         time.sleep(20)
         screenshot = driver.save_screenshot('/home/devel/kozienice_map.png')
-        print(t, " ScreenShoot: Mapa Kozienice")
-        
+        logging.info("ScreenShoot: Mapa Kozienice")
+            
         now = datetime.now()
         t = now.strftime("[%Y/%m/%d-%H:%M:%S]")
         driver.get('https://czujnikimiejskie.pl/public/piaseczno/?url_node_id=205&a='+str(milli_sec))
         time.sleep(20)
         screenshot = driver.save_screenshot('/home/devel/piaseczno_map.png')
-        print(t, " ScreenShoot: Mapa Piasecnzo")
-        
+        logging.info("ScreenShoot: Mapa Piaseczno")
+           
         now = datetime.now()
         t = now.strftime("[%Y/%m/%d-%H:%M:%S]")
         driver.get('https://czujnikimiejskie.pl/public/widgety/widget_kolno.html?id=281&a='+str(milli_sec))
         time.sleep(20)
         screenshot = driver.save_screenshot('/home/devel/widget_kolno.png')
-        print(t, " ScreenShoot: Widget Kolno")
-        
+        logging.info("ScreenShoot: Widget Kolno")
+            
         now = datetime.now()
         t = now.strftime("[%Y/%m/%d-%H:%M:%S]")
         driver.get('https://czujnikimiejskie.pl/public/widgety/widget_kozienice.html?id=196&a='+str(milli_sec))
         time.sleep(20)
         screenshot = driver.save_screenshot('/home/devel/widget_kozienice.png')
-        print(t, " ScreenShoot: Widget Kozienice")
-        
+        logging.info("ScreenShoot: Widget Kozienice")
+            
         now = datetime.now()
         t = now.strftime("[%Y/%m/%d-%H:%M:%S]")
         driver.get('https://czujnikimiejskie.pl/public/widgety/widget.html?id=205&a='+str(milli_sec))
         time.sleep(20)
         screenshot = driver.save_screenshot('/home/devel/widget_piaseczno.png')
-        print(t , " ScreenShoot: Widget Piaseczno")
-        
+        logging.info("ScreenShoot: Widget Piaseczno")
+            
         session = Session()
         response = session.post(
             url='http://czujnikimiejskie.pl/apipost/add/measurement',
-        data={"sn":"3000","a":"1","w":"0","z":"0"},
+            data={"sn":"3000","a":"1","w":"0","z":"0"},
                 headers={'Connection':'close'}
-             )
-        print("Wyslano")
+            )
+        logging.info("Wyslano")
         time.sleep(600)
-    except TimeoutException as e:
-        print("Wystepuje problem zwiazany z TimeoutException")
-        logging.debug("Wystepuje problem zwiazany z TimeoutException")
-        traceback.print_exc()  
+        driver.refresh()
+except TimeoutException as e:
+    logging.info("Wystepuje problem zwiazany z TimeoutException")
+    logging.debug("Wystepuje problem zwiazany z TimeoutException")
+    traceback.print_exc()  
+except Exception as e:
+    logging.info("Wystepuje nie znany problem")
+    logging.info("Sprawdz komenda df -h w folderze glownym / czy /dev/vda1 nie jest w pelni zajete")
+    traceback.print_exc()
 
 driver.quit()
 display.stop() 
